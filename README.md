7f03c8a802f37ab49505cc5d315200587edad482
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-04-24 23:53:29 -0500

    Create list devices tool

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is the [Pref-Editor](https://github.com/charlesmuchene/pref-editor-js.git) MCP server
 
+## Demo
+
+![list of tools](./demo-tools.png)
+
 ## License
 
 See [LICENSE](./LICENSE)
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { listDevices } from "@charlesmuchene/pref-editor";
-const main = async () => {
-  console.log("Application started: MCPing...");
-  const devices = await listDevices();
-  console.log(devices);
-  console.log("Application finished.");
-};
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-main().catch(console.error);
+const server = new McpServer({
+  name: "Pref-Editor",
+  version: "1.0.0",
+});
+
+server.tool("devices", "Lists connected devices", async () => ({
+  content: [
+    {
+      type: "text",
+      text: (await listDevices()).map((device) => device.serial).toString(),
+    },
+  ],
+}));
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
