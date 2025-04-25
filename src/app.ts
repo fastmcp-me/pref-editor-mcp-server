f8a812769bb3d2bc81f8c9d8dc2a9f832ba5ab3d
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-04-24 23:18:58 -0500

    Depend on pref-editor lib

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-const main = () => {
+import { listDevices } from "@charlesmuchene/pref-editor";
+const main = async () => {
   console.log("Application started: MCPing...");
+  const devices = await listDevices();
+  console.log(devices);
+  console.log("Application finished.");
 };
 
-main();
+main().catch(console.error);
