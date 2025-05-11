c89de4226bcbb080898a7e8f8a2de80dbc318b2c
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-05-11 15:39:40 -0500

    Bump app version

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,7 @@ const validate = (input: unknown, type: ZodType) => {
 const server = new McpServer(
   {
     name: "Pref-Editor",
-    version: "0.2.4",
+    version: "0.3.0",
   },
   {
     capabilities: {
